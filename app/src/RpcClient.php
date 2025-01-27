945cf6fc6916ad8af2c57fc337a87482d5ea295d
The Cryptory <108346802+lacryptorina@users.noreply.github.com>
2025-01-27 14:08:37 +0200

    Code generated by CodeCraftAI at 2025-01-27 12:08:22

---
+++ app/src/RpcClient.php
@@ -0,0 +1,9 @@
+<?php
+namespace SolanaBot;
+
+class RpcClient {
+    // Include the RpcClient implementation from the review
+    // [Include full RpcClient code here]
+}
+
+class RpcException extends \Exception {}
